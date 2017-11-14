d752140dd4670215dcfe24e7022c85d97354a9ea
Esteban Diaz <dexterprivate@msn.com>
2017-11-14 16:29:52 -0500

    add HousingOccupantsController,TenantsGenderController and their viewmodels

---
+++ HousingAPI/Controllers/APIControllers/HousingOccupantsController.cs
@@ -0,0 +1,28 @@
+﻿using HousingAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace HousingAPI.Controllers.APIControllers
+{
+    public class HousingOccupantsController : ApiController
+    {
+        private HousingDBEntities db = new HousingDBEntities();
+
+        [ResponseType(typeof(HousingUnit))]
+        public TenantsHousingUnitViewModel GetHousingOccupants(int id)
+        {
+            HousingUnit housingU = db.HousingUnits.Find(id);
+            List<Tenant> tenants_ = db.Tenants.Where(r => r.housingUnitId == housingU.housingUnitId).ToList();
+            TenantsHousingUnitViewModel occupants = new TenantsHousingUnitViewModel {
+                housingUnit = housingU,
+                tenants = tenants_  
+            };
+            return occupants;
+        }
+    }
+}
