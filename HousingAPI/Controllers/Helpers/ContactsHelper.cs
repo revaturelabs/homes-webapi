498bed4af9ab1cf4eb2385cddd4c886c39228328
Daniel Damudt <daniel.damudt@upr.edu>
2017-11-15 22:21:59 -0500

    Added contacts helper

--- HousingAPI/Controllers/Helpers/ContactsHelper.cs
+++ HousingAPI/Controllers/Helpers/ContactsHelper.cs
@@ -9,110 +9,57 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using HousingAPI.Models;
+using HousingAPI.Models.PresentationModels.Contact;
 
 namespace HousingAPI.Controllers.Helpers
 {
-    public class ContactsHelper : ApiController
+    public class ContactsHelper
     {
         private HousingDBEntities db = new HousingDBEntities();
 
-        // GET: api/Contacts
-        public IQueryable<Contact> GetContacts()
+        //Get all contacts
+        public IEnumerable<ContactMapper> GetContacts()
         {
-            return db.Contacts;
+            List<ContactMapper> cml = new List<ContactMapper>();
+            var toBeMapped = db.Contacts.ToList();
+            foreach (var contact in toBeMapped)
+            {
+                ContactMapper cm = new ContactMapper();
+
+                cm.ContactId = contact.contactId;
+                cm.Email = contact.email;
+                cm.FirstName = contact.firstName;
+                cm.LastName = contact.lastName;
+                cm.ObjectId = contact.objectId;
+                cm.PhoneNumber = contact.phoneNumber;
+
+                cml.Add(cm);
+            }
+
+            return cml;
         }
 
-        // GET: api/Contacts/5
-        [ResponseType(typeof(Contact))]
-        public IHttpActionResult GetContact(int id)
+        //Get single contact by id
+        public ContactMapper GetContact(int id)
         {
-            Contact contact = db.Contacts.Find(id);
+            var contact = db.Contacts.FirstOrDefault(i => i.contactId == id);
             if (contact == null)
             {
-                return NotFound();
+                ContactMapper cm = new ContactMapper();
+                return cm;
             }
-
-            return Ok(contact);
+            else
+            {
+                ContactMapper cm = new ContactMapper();
+                cm.ContactId = contact.contactId;
+                cm.Email = contact.email;
+                cm.FirstName = contact.firstName;
+                cm.LastName = contact.lastName;
+                cm.ObjectId = contact.objectId;
+                cm.PhoneNumber = contact.phoneNumber;
+                return cm;
+            }
         }
 
-        // PUT: api/Contacts/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutContact(int id, Contact contact)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != contact.contactId)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(contact).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ContactExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Contacts
-        [ResponseType(typeof(Contact))]
-        public IHttpActionResult PostContact(Contact contact)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Contacts.Add(contact);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = contact.contactId }, contact);
-        }
-
-        // DELETE: api/Contacts/5
-        [ResponseType(typeof(Contact))]
-        public IHttpActionResult DeleteContact(int id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-
-            db.Contacts.Remove(contact);
-            db.SaveChanges();
-
-            return Ok(contact);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool ContactExists(int id)
-        {
-            return db.Contacts.Count(e => e.contactId == id) > 0;
-        }
     }
 }
