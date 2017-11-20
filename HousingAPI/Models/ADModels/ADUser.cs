405be78e263cb7bbfda35fd9dd6821d2fcce7cf3
Daniel Damudt <daniel.damudt@upr.edu>
2017-11-20 09:46:57 -0500

    Added ADModels

---
+++ HousingAPI/Models/ADModels/ADUser.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HousingAPI.Models.ADModels
+{
+    public class ADUserResponseModel
+    {
+        public string ObjectId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class ADUserList
+    {
+        public List<ADUserJsonModel> ADUserJsonModel { get; set; }
+    }
+
+    public class ADUserJsonModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("businessPhones")]
+        public List<string> BusinessPhones { get; set; }
+
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("givenName")]
+        public string GivenName { get; set; }
+
+        [JsonProperty("jobTitle")]
+        public string JobTitle { get; set; }
+
+        [JsonProperty("mail")]
+        public string Mail { get; set; }
+
+        [JsonProperty("mobilePhone")]
+        public string MobilePhone { get; set; }
+
+        [JsonProperty("officeLocation")]
+        public string OfficeLocation { get; set; }
+
+        [JsonProperty("preferredLanguage")]
+        public string PreferredLanguage { get; set; }
+
+        [JsonProperty("surname")]
+        public string surname { get; set; }
+
+        [JsonProperty("userPrincipalName")]
+        public string UserPrincipalName { get; set; }
+    }
+}
