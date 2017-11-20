7908683fd35481a94b939703e5b992c9d80546aa
Daniel Damudt <daniel.damudt@upr.edu>
2017-11-20 15:49:41 -0500

    Changed model, added get method for ADUsers and get access token functionality

--- HousingAPI/Controllers/GraphAPIControllers/ADUsersController.cs
+++ HousingAPI/Controllers/GraphAPIControllers/ADUsersController.cs
@@ -20,24 +20,54 @@ namespace HousingAPI.Controllers.GraphAPIControllers
 
     public class ADUsersController : ApiController
     {
+        public ADUserGraphTokenResponse GenerateAccessToken()
+        {
+            ADUserGraphTokenResponse aDUserGraphTokenResponse = new ADUserGraphTokenResponse();
+
+            HttpClient tokenClient = new HttpClient();
+
+            Task.Run(async () =>
+            {
+                tokenClient.BaseAddress = new Uri("https://login.microsoftonline.com/" + ConfigurationManager.AppSettings["ida:Tenant"] + "/oauth2/v2.0/token");
+                var content = new FormUrlEncodedContent(new[]
+                {
+                new KeyValuePair<string, string>("tenant", ConfigurationManager.AppSettings["ida:Tenant"]),
+                new KeyValuePair<string, string>("client_id", ConfigurationManager.AppSettings["ida:Audience2"]),
+                new KeyValuePair<string, string>("scope", "https://graph.microsoft.com/.default"),
+                new KeyValuePair<string, string>("client_secret", ConfigurationManager.AppSettings["ida:Secret2"]),
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            });
+                var result = await tokenClient.PostAsync("", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                JToken parsedResult = JToken.Parse(resultContent);
+
+                aDUserGraphTokenResponse.AccessToken = parsedResult["access_token"].Value<string>();
+                aDUserGraphTokenResponse.ExpiresIn = parsedResult["expires_in"].Value<string>();
+                aDUserGraphTokenResponse.TokenType = parsedResult["token_type"].Value<string>();
+
+            }).Wait();
+
+            return aDUserGraphTokenResponse;
+        }
+
         // Get users in active directory
         [Authorize]
         [HttpGet]
         [ResponseType(typeof(ADUserJsonModel))]
-        public IEnumerable<ADUserJsonModel> GetADUsers()
+        public JToken GetADUsers()
         {
-            string responseString = "";
-            string token = Request.Headers.Authorization.Parameter;
+            ADUserGraphTokenResponse aDUserGraphTokenResponse = GenerateAccessToken();
 
-            HttpClient client = new HttpClient();
+            HttpClient graphCRUDClient = new HttpClient();
+            string responseString = "";
 
             Task.Run(async () =>
             {
                 HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/users");
 
-                message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Authorization", "Bearer " + token);
+                message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", aDUserGraphTokenResponse.AccessToken);
 
-                HttpResponseMessage response = await client.SendAsync(message);
+                HttpResponseMessage response = await graphCRUDClient.SendAsync(message);
                 responseString = await response.Content.ReadAsStringAsync();
             }).Wait();
 
@@ -45,15 +75,21 @@ namespace HousingAPI.Controllers.GraphAPIControllers
             {
                 JToken parsed = JToken.Parse(responseString);
 
-                //Parse response into appropiate model
-                ADGetUsersJSONResponseModel aDUserList = new ADGetUsersJSONResponseModel()
-                {
-                    Context = parsed["@odata.context"].Value<string>(),
-                    ADUserJsonModel = parsed["value"].Value<List<ADUserJsonModel>>()
+                ////Parse response into appropiate model
+                //ADGetUsersJSONResponseModel aDUserList = new ADGetUsersJSONResponseModel()
+                //{
+                //    Context = parsed["@odata.context"].Value<string>(),
+                //    //ADUserJsonModel = parsed["value"].Value<List<ADUserJsonModel>>()
 
-                };
+                //};
 
-                return aDUserList.ADUserJsonModel;
+                //foreach (var item in parsed["value"].Value<List<ADUserJsonModel>>())
+                //{
+                //    aDUserList.ADUserJsonModel.Add(item);
+                //}
+
+                //return aDUserList.ADUserJsonModel;
+                return parsed;
 
             }
             catch
--- HousingAPI/Models/ADModels/ADUser.cs
+++ HousingAPI/Models/ADModels/ADUser.cs
@@ -6,6 +6,26 @@ using System.Web;
 
 namespace HousingAPI.Models.ADModels
 {
+    public class ADUserGraphTokenResponse
+    {
+        //[JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        ////[JsonProperty("scope")]
+        //public string Scope { get; set; }
+
+        //[JsonProperty("expires_in")]
+        public string ExpiresIn { get; set; }
+
+        ////[JsonProperty("ext_expires_in")]
+        //public string ExtExpiresIn { get; set; }
+
+        //[JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        ////[JsonProperty("refresh_token")]
+        //public string RefreshToken { get; set; }
+    }
     public class ADUserResponseModel
     {
         public string ObjectId { get; set; }
