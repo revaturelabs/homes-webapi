765040bc46d4bef049187de3d92704a7fde8ecb4
Daniel Damudt <daniel.damudt@upr.edu>
2017-11-22 16:49:41 -0500

    Email system is up

---
+++ HousingAPI/Controllers/Helpers/EmailHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mail;
+
+namespace HousingAPI.Controllers.Helpers
+{
+    class EMailHelper
+    {
+        public void SendMail(string recipient, string body)
+        {
+            string pGmailEmail = "revature.housing@gmail.com";
+            string pGmailPassword = "DotNet1709";
+            string pTo = recipient; //abc@domain.com
+            string pSubject = "Revature Housing Account";
+            string pBody = body; //Body
+            MailFormat pFormat = MailFormat.Text; //Text Message
+            string pAttachmentPath = string.Empty; //No Attachments
+
+            System.Web.Mail.MailMessage myMail = new System.Web.Mail.MailMessage();
+            myMail.Fields.Add
+                ("http://schemas.microsoft.com/cdo/configuration/smtpserver",
+                              "smtp.gmail.com");
+            myMail.Fields.Add
+                ("http://schemas.microsoft.com/cdo/configuration/smtpserverport",
+                              "465");
+            myMail.Fields.Add
+                ("http://schemas.microsoft.com/cdo/configuration/sendusing",
+                              "2");
+            //sendusing: cdoSendUsingPort, value 2, for sending the message using 
+            //the network.
+
+            //smtpauthenticate: Specifies the mechanism used when authenticating 
+            //to an SMTP 
+            //service over the network. Possible values are:
+            //- cdoAnonymous, value 0. Do not authenticate.
+            //- cdoBasic, value 1. Use basic clear-text authentication. 
+            //When using this option you have to provide the user name and password 
+            //through the sendusername and sendpassword fields.
+            //- cdoNTLM, value 2. The current process security context is used to 
+            // authenticate with the service.
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate", "1");
+            //Use 0 for anonymous
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/sendusername",
+                pGmailEmail);
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/sendpassword",
+                 pGmailPassword);
+            myMail.Fields.Add
+            ("http://schemas.microsoft.com/cdo/configuration/smtpusessl",
+                 "true");
+            myMail.From = pGmailEmail;
+            myMail.To = pTo;
+            myMail.Subject = pSubject;
+            myMail.BodyFormat = pFormat;
+            myMail.Body = pBody;
+            if (pAttachmentPath.Trim() != "")
+            {
+                MailAttachment MyAttachment =
+                        new MailAttachment(pAttachmentPath);
+                myMail.Attachments.Add(MyAttachment);
+                myMail.Priority = System.Web.Mail.MailPriority.High;
+            }
+
+            SmtpMail.SmtpServer = "smtp.gmail.com:465";
+            SmtpMail.Send(myMail);
+        }
+    }
+}
